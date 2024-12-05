265313e00f43e6cc65f7fc72d074219a86975207
Felixburd <felixburt@gmail.com>
2024-12-04 23:19:00 -0500

    Update local changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-};
-
-module.exports = nextConfig;
+    images: {
+      domains: ['localhost'],
+    },
+  }
+  
+  module.exports = nextConfig
